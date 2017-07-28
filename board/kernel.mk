d5e9806840e7f902da997cb6da5f23bf0dd96dac
Joe Maples <joe@frap129.org>
2017-07-28 15:34:07 -0500

    ether: Import Vox Populi and add EAS support
    Change-Id: I40452e58fcb7ac7a3ae4eee141ca9205a50dc08b
    Signed-off-by: Joe Maples <joe@frap129.org>

--- board/kernel.mk
+++ board/kernel.mk
@@ -4,12 +4,14 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 
 TARGET_KERNEL_SOURCE := kernel/nextbit/msm8992
-TARGET_KERNEL_CONFIG := carbon_ether_defconfig
+TARGET_KERNEL_CONFIG := ether_eas_defconfig
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
 ENABLE_SCHED_BOOST := true
 
 TARGET_USES_64_BIT_BINDER := true
+TARGET_USES_INTERACTION_BOOST := true
