e650d25fce36326cd5c8a898b6130f6a5c6b1ebe
Joe Maples <joe@frap129.org>
2017-09-04 20:37:36 -0400

    ether: Add HIDL Manifest and HALs
    Change-Id: I962999e2c6ac323018ed3f569f9662d806af4106
    Signed-off-by: Joe Maples <joe@frap129.org>

--- product/qcom-media.mk
+++ product/qcom-media.mk
@@ -8,8 +8,12 @@ PRODUCT_PACKAGES += \
     libOmxVdecHevc \
     libOmxVenc
 
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 PRODUCT_PROPERTY_OVERRIDES += \
     mm.enable.smoothstreaming=true \
     media.aac_51_output_enabled=true \
     vidc.debug.level=1 \
-    vidc.enc.dcvs.extra-buff-count=2
+    vidc.enc.dcvs.extra-buff-count=2 \
+    persist.media.treble_omx=false
