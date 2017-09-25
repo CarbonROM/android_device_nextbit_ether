ac978ad935d6962750dca5666d89b905f1028f1f
Joe Maples <joe@frap129.org>
2017-09-24 21:48:12 -0400

    ether: Copy fstab with PRODUCT_COPY_FILES
    Change-Id: Ib3bbad65a251dbd39675402f09874e554a49439d
    Signed-off-by: Joe Maples <joe@frap129.org>

--- product/platform.mk
+++ product/platform.mk
@@ -1,8 +1,8 @@
 PRODUCT_PACKAGES += \
-    fstab.qcom \
     init.qcom.rc \
     ueventd.qcom.rc
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    device/nextbit/ether/rootdir/fstab.qcom:root/fstab.qcom
